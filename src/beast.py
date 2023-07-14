a6a861a12d29c95999f9a043e5b297a9c834914a
Miloslav Macůrek <macurek.m@gmail.com>
2023-07-14 18:35:27 +0200

    Replace ' for "

--- src/beast.py
+++ src/beast.py
@@ -9,8 +9,8 @@ from search import main as search_main, Node
 
 
 # VARIABLES
-engineName = 'Beast 1.01'
-author = 'Miloslav Macurek'
+engineName = "Beast 1.01"
+author = "Miloslav Macurek"
 
 
 # CLASSES
@@ -18,7 +18,7 @@ class GoParameters:
     def __init__(self):
         # position
         self.moves = ""
-        self.root = Node('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+        self.root = Node("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 
         # time parameters
         self.wtime = None
@@ -57,49 +57,49 @@ class Options:
         self.debug = False						# debug option
         self.threads = 1						# number of threads
         self.fiftyMoveRule = True				# whether to play with 50-move rule or not
-        self.syzygyPath = '<empty>'				# path to syzygy tablebases
+        self.syzygyPath = "<empty>"				# path to syzygy tablebases
         self.syzygyProbeLimit = 6				# probe limit for syzygy tablebases
         self.timeFlex = 10						# time flex for time management
-        self.searchAlgorithm = 'AlphaBeta'		# search algorithm
+        self.searchAlgorithm = "AlphaBeta"		# search algorithm
         self.flag = Event()						# flag to start go function
         self.quiescence = True
-        self.heuristic = 'Classic'				# type of heuristic
-        self.network = 'Regression'				# type of neural network system
-        self.modelFile = 'nets/bnn_2-100-2.h5'
+        self.heuristic = "Classic"				# type of heuristic
+        self.network = "Regression"				# type of neural network system
+        self.modelFile = "nets/bnn_2-100-2.h5"
         self.model = None
 
     def set(self, option, value):
-        if option in ['debug', 'Debug'] and value in ['on', 'On']:
+        if option in ["debug", "Debug"] and value in ["on", "On"]:
             self.debug = True
-        elif option in ['debug', 'Debug'] and value in ['off', 'Off']:
+        elif option in ["debug", "Debug"] and value in ["off", "Off"]:
             self.debug = False
-        elif (option in ["threads", 'Threads']) and (int(value) < 32) and (int(value) > 0):
+        elif (option in ["threads", "Threads"]) and (int(value) < 32) and (int(value) > 0):
             self.threads = value
-        elif option == 'Syzygy50MoveRule':
-            if value in ['true', 'True', '1']:
+        elif option == "Syzygy50MoveRule":
+            if value in ["true", "True", "1"]:
                 self.fiftyMoveRule = True
-            elif value in ['false', 'False', '0']:
+            elif value in ["false", "False", "0"]:
                 self.fiftyMoveRule = False
-        elif option in ['timeflex', 'TimeFlex']:
+        elif option in ["timeflex", "TimeFlex"]:
             self.timeFlex = value
-        elif option in ['SearchAlgorithm', 'searchalgorithm']:
+        elif option in ["SearchAlgorithm", "searchalgorithm"]:
             self.searchAlgorithm = value
-        elif option in ['SyzygyPath', 'syzzygypath']:
+        elif option in ["SyzygyPath", "syzzygypath"]:
             self.syzygyPath = value.replace('\\', '/')
-        elif option in ['SyzygyProbeLimit', 'syzygyprobelimit']:
+        elif option in ["SyzygyProbeLimit", "syzygyprobelimit"]:
             self.syzygyProbeLimit = int(value)
-        elif option in ['quiescence', 'Quiescence']:
-            if value in ['true', 'True', '1']:
+        elif option in ["quiescence", "Quiescence"]:
+            if value in ["true", "True", "1"]:
                 self.quiescence = True
-            elif value in ['false', 'False', '0']:
+            elif value in ["false", "False", "0"]:
                 self.quiescence = False
-        elif option in ['Heuristic', 'heuristic']:
+        elif option in ["Heuristic", "heuristic"]:
             self.heuristic = value
-            if value in ['random', 'Random']:
+            if value in ["random", "Random"]:
                 go_params.depth = 1
-        elif option in ['network', 'Network']:
+        elif option in ["network", "Network"]:
             self.network = value
-        elif option in ['modelfile', 'ModelFile']:
+        elif option in ["modelfile", "ModelFile"]:
             self.modelFile = value.replace('\\', '/')
 
     def value(self, option):
@@ -107,63 +107,63 @@ class Options:
             return self.debug
         elif option == "threads":
             return self.threads
-        elif option in ['SyzygyPath', 'syzygypath']:
+        elif option in ["SyzygyPath", "syzygypath"]:
             return self.syzygyPath
-        elif option in ['SyzygyProbeLimit', 'syzygyprobelimit']:
+        elif option in ["SyzygyProbeLimit", "syzygyprobelimit"]:
             return self.syzygyProbeLimit
-        elif option in ['quiescence', 'Quiescence']:
+        elif option in ["quiescence", "Quiescence"]:
             return self.quiescence
-        elif option in ['heuristic', 'Heuristic']:
+        elif option in ["heuristic", "Heuristic"]:
             return self.heuristic
-        elif option in ['Network', 'network']:
+        elif option in ["Network", "network"]:
             return self.network
-        elif option in ['modelfile', 'ModelFile']:
+        elif option in ["modelfile", "ModelFile"]:
             return self.modelFile
 
 
 class UciLoop(Cmd):
-    prompt = ''
+    prompt = ""
 
     def do_uci(self, arg):
         if opt.debug:
-            f = open('log.txt', 'a')
-            f.write('uci ')
+            f = open("log.txt", "a")
+            f.write("uci ")
             f.write(arg)
             f.write('\n')
             f.close()
-        print('id name', engineName)
-        print('id author', author)
+        print("id name", engineName)
+        print("id author", author)
         print()
-        print('option name Threads type spin default',
-              opt.threads, 'min 1 max 1')
+        print("option name Threads type spin default",
+              opt.threads, "min 1 max 1")
         # time flexibility in ms so engine could make a move in time and did not lose on time
-        print('option name TimeFlex type spin default',
-              opt.timeFlex, 'min 0 max 1000')
+        print("option name TimeFlex type spin default",
+              opt.timeFlex, "min 0 max 1000")
         # types of search algorithms
-        print('option name SearchAlgorithm type combo default',
-              opt.searchAlgorithm, 'var AlphaBeta')
-        print('option name Quiescence type check default',
+        print("option name SearchAlgorithm type combo default",
+              opt.searchAlgorithm, "var AlphaBeta")
+        print("option name Quiescence type check default",
               opt.quiescence)
         # path to syzygy tablebases
-        print('option name SyzygyPath type string default',
+        print("option name SyzygyPath type string default",
               opt.syzygyPath)
         # probe limit for syzygy
-        print('option name SyzygyProbeLimit type spin default',
-              opt.syzygyProbeLimit, 'min 0 max 7')
-        print('option name Syzygy50MoveRule type check default',
+        print("option name SyzygyProbeLimit type spin default",
+              opt.syzygyProbeLimit, "min 0 max 7")
+        print("option name Syzygy50MoveRule type check default",
               opt.fiftyMoveRule)
-        print('option name Heuristic type combo default',
-              opt.heuristic, 'var Classic var NeuralNetwork var Random')
-        print('option name Network type combo default',
-              opt.network, 'var Regression var Classification')
-        print('option name ModelFile type string default',
+        print("option name Heuristic type combo default",
+              opt.heuristic, "var Classic var NeuralNetwork var Random")
+        print("option name Network type combo default",
+              opt.network, "var Regression var Classification")
+        print("option name ModelFile type string default",
               opt.modelFile)
-        print('uciok')
+        print("uciok")
 
     def do_quit(self, arg):
         if opt.debug:
-            f = open('log.txt', 'a')
-            f.write('quit ')
+            f = open("log.txt", "a")
+            f.write("quit ")
             f.write(arg)
             f.write('\n')
             f.close()
@@ -171,8 +171,8 @@ class UciLoop(Cmd):
 
     def do_setoption(self, arg):
         if opt.debug:
-            f = open('log.txt', 'a')
-            f.write('setoption ')
+            f = open("log.txt", "a")
+            f.write("setoption ")
             f.write(arg)
             f.write('\n')
             f.close()
@@ -182,33 +182,33 @@ class UciLoop(Cmd):
             print(command[1], " = ", opt.value(command[1]))
 
     def do_debug(self, arg):
-        opt.set('debug', arg)
+        opt.set("debug", arg)
 
     def do_isready(self, arg):
         if opt.debug:
-            f = open('log.txt', 'a')
-            f.write('isready ')
+            f = open("log.txt", "a")
+            f.write("isready ")
             f.write(arg)
             f.write('\n')
             f.close()
-        print('readyok')
+        print("readyok")
 
     def do_go(self, arg):
         if opt.debug:
-            f = open('log.txt', 'a')
-            f.write('go ')
+            f = open("log.txt", "a")
+            f.write("go ")
             f.write(arg)
             f.write('\n')
             f.close()
-        if arg.startswith('ponder'):
+        if arg.startswith("ponder"):
             return
         task.put(arg)
         opt.flag.set()
 
     def do_stop(self, arg):
         if opt.debug:
-            f = open('log.txt', 'a')
-            f.write('stop ')
+            f = open("log.txt", "a")
+            f.write("stop ")
             f.write(arg)
             f.write('\n')
             f.close()
@@ -216,17 +216,17 @@ class UciLoop(Cmd):
 
     def do_ucinewgame(self, arg):
         if opt.debug:
-            f = open('log.txt', 'a')
-            f.write('ucinewgame ')
+            f = open("log.txt", "a")
+            f.write("ucinewgame ")
             f.write(arg)
             f.write('\n')
             f.close()
-        go_params.position = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+        go_params.position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
     def do_position(self, arg):
         if opt.debug:
-            f = open('log.txt', 'a')
-            f.write('position ')
+            f = open("log.txt", "a")
+            f.write("position ")
             f.write(arg)
             f.write('\n')
             f.close()
@@ -236,16 +236,16 @@ class UciLoop(Cmd):
             command = arg
             arguments = None
 
-        if command == 'startpos' and arguments:
+        if command == "startpos" and arguments:
             arguments = arguments.split()
-            fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+            fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
             moves = arguments[1:]
         elif command and not arguments:
-            fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+            fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
             moves = None
         else:
             try:
-                [fen, moves] = arguments.split(' moves ')
+                [fen, moves] = arguments.split(" moves ")
                 moves = moves.split()
             except ValueError:
                 fen = arguments
@@ -263,7 +263,7 @@ def get_root(fen, moves):
     if moves is not None:
         for move in moves:
             fen = board.fen().split(' ')
-            root.previous.append(' '.join(fen[:2]))
+            root.previous.append(" ".join(fen[:2]))
             board.push(Move.from_uci(move))
     root.position = board.fen()
     return root
@@ -272,13 +272,13 @@ def get_root(fen, moves):
 def parse_params(go_params, string):
     run = True
     while run:
-        if string == 'infinite':
+        if string == "infinite":
             go_params.infinite = True
             break
-        elif string == 'ponder':
+        elif string == "ponder":
             go_params.ponder = True
             break
-        elif string == '':
+        elif string == "":
             go_params.depth = 2
             break
         try:
@@ -287,23 +287,23 @@ def parse_params(go_params, string):
             [command, param] = string.split(' ', 1)
             run = False
 
-        if command == 'wtime':
+        if command == "wtime":
             go_params.wtime = int(param)
-        elif command == 'btime':
+        elif command == "btime":
             go_params.btime = int(param)
-        elif command == 'winc':
+        elif command == "winc":
             go_params.winc = int(param)
-        elif command == 'binc':
+        elif command == "binc":
             go_params.binc = int(param)
-        elif command == 'movestogo':
+        elif command == "movestogo":
             go_params.movesToGo = int(param)
-        elif command == 'depth':
+        elif command == "depth":
             go_params.depth = int(param)
-        elif command == 'nodes':
+        elif command == "nodes":
             go_params.nodes = int(param)
-        elif command == 'mate':
+        elif command == "mate":
             go_params.mate = int(param)
-        elif command == 'movetime':
+        elif command == "movetime":
             go_params.movetime = int(param)
 
 
@@ -317,7 +317,7 @@ def go():
 
 
 # MAIN + INITIALIZATIONS
-if __name__ == '__main__':
+if __name__ == "__main__":
     # do not log unnecessary tensorflow messages
     environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
     # disable GPU for model evaluation even if available (slower than cpu for small beast nets)
@@ -330,7 +330,7 @@ if __name__ == '__main__':
     worker = Thread(target=go)			# worker thread
     worker.daemon = True				# stop when main thread stops
 
-    print(engineName, 'by', author)
+    print(engineName, "by", author)
 
     worker.start()
     UciLoop().cmdloop()
