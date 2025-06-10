93ce66b63d663b7e7f7789bbd74792442b29a905
Miloslav Macůrek <macurek.m@gmail.com>
2025-06-10 10:29:28 +0200

    Fix macOS support

--- src/beast_chess/main.py
+++ src/beast_chess/main.py
@@ -5,16 +5,20 @@ from beast_chess.engine import Engine, UciProtocol
 from beast_chess.infra import Constants
 
 
-def main() -> None:
-    print(f"{Constants.ENGINE_NAME} {Constants.ENGINE_VERSION} by {Constants.AUTHOR}")
-
-    queue = Queue()
-    Process(target=Engine(queue).start, daemon=True).start()
-    sleep(0.1)  # wait for the process to start
-
-    UciProtocol(queue).uci_loop()
+def start_engine(command_queue: Queue) -> None:
+    """
+    Safely start the engine after creating its own new process.
+    Necessary for macOS compatibility.
+    """
+    Engine(command_queue).start()
 
 
 if __name__ == "__main__":
+    print(f"{Constants.ENGINE_NAME} {Constants.ENGINE_VERSION} by {Constants.AUTHOR}")
+
     freeze_support()
-    main()
+    queue = Queue()
+    Process(target=start_engine, args=(queue,), daemon=True).start()
+    sleep(0.1)  # wait for the process to start
+
+    UciProtocol(queue).uci_loop()
